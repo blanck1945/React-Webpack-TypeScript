1856ff30e7737781fa5da8ac4819ba952dd13468
Okubo <aspastrana990@gmail.com>
2020-08-12 16:36:04 -0300

    Adding Redux init to template

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import * as reactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./main.scss";
 import Navbar from "./layouts/Navbar/navbar";
 import Footer from "./layouts/Footer/Footer";
 import HomeNavbar from "./components/home_navbar/HomeNavbar";
+import { Provider } from "react-redux";
+import { store as Redux_Store } from "./Redux_Store/store";
+import "./main.scss";
 
 const App: React.FunctionComponent = () => {
   return (
@@ -19,10 +21,12 @@ const App: React.FunctionComponent = () => {
 };
 
 reactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <Provider store={Redux_Store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
--- webpack.common.js
+++ webpack.common.js
@@ -52,4 +52,7 @@ module.exports = {
         ]
     },
     devtool: "cheap-module-eval-source-map",
+    devServer: {
+        historyApiFallback: true,
+    },
 }
